a5ff587e72b54ad2c93681b28f87ce5f3bc756ea
navuhod <navuh@mail.ru>
2018-04-04 18:05:14 -0400

    Раздел 3. Исправление файла sample_print.py

--- 03_/sample_print.py
+++ 03_/sample_print.py
@@ -1,23 +1,4 @@
-#tunnel = """
-#interface Tunnel0
-#ip address 10.10.10.1 255.255.255.0
-#ip mtu 1416
-#ip ospf hello-interval 5
-#tunnel source FastEthernet1/0
-#tunnel protection opsec profile DMVPN
-#"""
-#print(tunnel)
-
-# строки то УПОРЯДОЧЕННЫЙ тип данных, позволяющий обращаться к символам в строке по номеру, начиная с нуля
-strok = 'абвгдежзиклмнопрстуфхцчшыэюя'
-print('пример строки набранной по-символьно с переменной strok="абвгдежзиклмнопрстуфхцчшыэюя":', strok[17]+strok[-4], end=' ')
-print(strok[12]+strok[8]+strok[9]+strok[8]+strok[-11]+strok[0], end=' ')
-print(strok[25]+strok[-11]+strok[13], end=' ')
-print(strok[-11]+strok[13]+strok[6]+strok[5], end=' ')
-print(strok[11]+strok[13]+strok[6]+strok[5]+strok[-5]+'ь')
-print()
-print('напечатанной вот таким "диким" способом: '), print('''print('пример строки набранной по-символьно с переменной strok="абвгдежзиклмнопрстуфхцчшыэюя":', strok[17]+strok[-4], end=' ')
-print(strok[12]+strok[8]+strok[9]+strok[8]+strok[-11]+strok[0], end=' ')
-print(strok[25]+strok[-11]+strok[13], end=' ')
-print(strok[-11]+strok[13]+strok[6]+strok[5], end=' ')
-print(strok[11]+strok[13]+strok[6]+strok[5]+strok[-5]+'ь')''')
+a = [1, 2, 3, 4, 5]
+print(*a, sep'_'*10, end=' gotta!', '\n'*2)
+print('one'), print('two'), print()
+print('one', end=''), print('two)'
